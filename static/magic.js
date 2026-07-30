79ecdcf118dfd0d1e7b7fbff252b6798d423c663
bradnielsen2981 <brad_nielsen29@hotmail.com>
2026-07-30 10:36:42 +1000

    commit

--- static/magic.js
+++ static/magic.js
@@ -7,9 +7,9 @@ ballimage.onclick = magic8ball;
 
 clickme.onclick = magic8ball;
 
-function magic8ball()
-{
+function magic8ball() { 
     num = Math.floor(Math.random() * 5);
     answerbox.innerHTML = "<b>" + answers[num] + "</b>";
+    answerbox.style.backgroundColor = "yellow";
 }
 
