833cd388f76c88b6a95cb1dd405b33635d6e9dd5
bradnielsen2981 <brad_nielsen29@hotmail.com>
2026-07-30 10:17:49 +1000

    commit

--- static/magic.js
+++ static/magic.js
@@ -3,10 +3,13 @@ alert("Can you feel the magic?");
 answers = ["Yes", "No", "Maybe", 
     "Ask again later", "Definitely not"];
 
+ballimage.onclick = magic8ball;
+
+clickme.onclick = magic8ball;
+
 function magic8ball()
 {
     num = Math.floor(Math.random() * 5);
-    return answers[num];
+    answerbox.innerHTML = "<b>" + answers[num] + "</b>";
 }
 
-console.log(magic8ball());
