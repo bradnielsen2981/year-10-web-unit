e5628abf03bd8a8cbd2d3cdf9f3fb30b0636e9c2
bradnielsen2981 <brad_nielsen29@hotmail.com>
2026-08-03 09:05:32 +1000

    commit

--- static/magic.js
+++ static/magic.js
@@ -4,12 +4,9 @@ answers = ["Yes", "No", "Maybe",
     "Ask again later", "Definitely not"];
 
 ballimage.onclick = magic8ball;
-
 clickme.onclick = magic8ball;
-
 function magic8ball() { 
     num = Math.floor(Math.random() * 5);
     answerbox.innerHTML = "<b>" + answers[num] + "</b>";
-    answerbox.style.backgroundColor = "yellow";
 }
 
